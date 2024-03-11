7e63013b2f1149e420e0c10090d89ce70faba70a
Erick Adriél Garcia <134140333+erickadrielgarcia@users.noreply.github.com>
2024-03-11 15:40:13 -0300

    Update Valor de faturamento por empresa..sql

--- Valor de faturamento por empresa..sql
+++ Valor de faturamento por empresa..sql
@@ -1,1 +1,4 @@
-SELECT id_empresa, SUM(valor_total) FROM public.pedido GROUP BY id_empresa
+SELECT p.id_empresa, e.razao_social, SUM(valor_total) 
+FROM public.pedido p 
+JOIN public.empresa e ON e.id_empresa = p.id_empresa
+GROUP BY p.id_empresa, e.razao_social
