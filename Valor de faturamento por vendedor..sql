3c2fb24185aa5f843055060f9669cd1c6e4796ad
Erick Adriél Garcia <134140333+erickadrielgarcia@users.noreply.github.com>
2024-03-11 15:41:15 -0300

    Update Valor de faturamento por vendedor..sql

--- Valor de faturamento por vendedor..sql
+++ Valor de faturamento por vendedor..sql
@@ -1,6 +1,5 @@
 SELECT v.id_vendedor,v.nome,
-/*Soma do preço praticado multiplicado pela quantidade de cada item do pedido, 
-resultando no valor total de vendas para cada vendedor*/
+-- Soma do preço praticado multiplicado pela quantidade de cada item do pedido, resultando no valor total de vendas para cada vendedor
 SUM(ip.preco_praticado * ip.quantidade) AS valor_total 
 FROM public.vendedores v
 JOIN public.clientes c ON v.id_vendedor = c.id_vendedor
@@ -8,4 +7,3 @@ JOIN public.pedido p ON c.id_cliente = p.id_cliente
 JOIN public.itens_pedido ip ON p.id_pedido = ip.id_pedido 
 GROUP BY  v.id_vendedor, v.nome 
 ORDER BY v.nome ASC
-
